71b60235cc86df1a6a633030219b1cce589d6bf9
Malte Riesch <malteriesch@googlemail.com>
2013-11-10 01:24:34 +0000

    refactored

--- lib/TestDbAcle/Db/DataInserter.php
+++ lib/TestDbAcle/Db/DataInserter.php
@@ -2,7 +2,8 @@
 
 namespace TestDbAcle\Db;
 
-class DataInserter {
+class DataInserter
+{
 
     protected $tableInfo;
 
@@ -11,87 +12,84 @@ class DataInserter {
      */
     private $pdo;
 
-    public function __construct(\PDO $pdo = null) {
+    public function __construct(\PDO $pdo = null)
+    {
         $this->pdo = $pdo;
         $this->tableInfo = new TableInfo();
     }
 
-    public function process($dataTree) {
-        
+    public function process($dataTree)
+    {
+
         foreach (array_keys($dataTree) as $tableName) {
-            $this->tableInfo->addTableDescription($tableName,$this->getQuery('describe ' . $tableName));
+            $this->tableInfo->addTableDescription($tableName, $this->getQuery('describe ' . $tableName));
         }
-        
-        foreach ($dataTree as $tableName => $content) { 
-            $this->processTable($tableName, $content['data'],$content['meta']);
+
+        foreach ($dataTree as $tableName => $content) {
+            $this->processTable($tableName, $content['data'], $content['meta']);
         }
     }
 
-
-    public function processTable($tableName, $content, $meta) {
-        if (isset($meta['mode']) && $meta['mode']=='replace'){
-            $this->replaceIntoTable($tableName, $content,$meta['identifiedBy']);
+    public function processTable($tableName, $content, $meta)
+    {
+        if (isset($meta['mode']) && $meta['mode'] == 'replace') {
+            $this->replaceIntoTable($tableName, $content, $meta['identifiedBy']);
         } else {
             $this->clearAndInsertTable($tableName, $content);
         }
     }
-    
-    public function replaceIntoTable($tableName, $content,$identifiedBy) {
-        
 
-        foreach ($content as $valuesToBeInserted) {
-            
-            $updateBuilder      = new Sql\UpdateBuilder($tableName);
-            foreach ($identifiedBy as $idColumn){
-                $updateBuilder->addCondition("$idColumn='{$valuesToBeInserted[$idColumn]}'");
-            }
-            
-            $conditionString = $updateBuilder->getConditionSql();
-            $numberOfRecords = $this->getQuery("SELECT COUNT(*) N FROM $tableName WHERE $conditionString");
-            if ($numberOfRecords[0]['N']==0)
-            {
-                $updateBuilder = new Sql\InsertBuilder($tableName);
-            }
+    protected function getUpserter($tableName,$identifiedBy,$valuesToBeInserted)
+    {
+        $updateBuilder = new Sql\UpdateBuilder($tableName);
+        foreach ($identifiedBy as $idColumn) {
+            $updateBuilder->addCondition("$idColumn='{$valuesToBeInserted[$idColumn]}'");
+        }
 
-            foreach ($this->tableInfo->getDecorateWithNullPlaceHolders($tableName, $valuesToBeInserted) as $columnName => $columnValue) {
-                if ($columnValue == 'NULL' && $this->tableInfo->isNullable($tableName,$columnName)) {
-                    $updateBuilder->addColumn($columnName, 'NULL', true);
-                } else {
-                    $updateBuilder->addColumn($columnName, $columnValue);
-                }
-            }
-            $this->executeSql($updateBuilder->GetSql());
+        $conditionString = $updateBuilder->getConditionSql();
+        $numberOfRecords = $this->getQuery("SELECT COUNT(*) N FROM $tableName WHERE $conditionString");
+        if ($numberOfRecords[0]['N'] == 0) {
+            return new Sql\InsertBuilder($tableName);
         }
         
+        return $updateBuilder;
         
     }
+
+    protected function insertValues($upsertBuilder,$tableName,$valuesToBeInserted)
+    {
+        foreach ($this->tableInfo->getDecorateWithNullPlaceHolders($tableName, $valuesToBeInserted) as $columnName => $columnValue) {
+            if ($columnValue == 'NULL' && $this->tableInfo->isNullable($tableName, $columnName)) {
+                $upsertBuilder->addColumn($columnName, 'NULL', true);
+            } else {
+                $upsertBuilder->addColumn($columnName, $columnValue);
+            }
+        }
+        $this->executeSql($upsertBuilder->GetSql());
+    }
     
-    public function clearAndInsertTable($tableName, $content) {
-        
+    public function replaceIntoTable($tableName, $content, $identifiedBy)
+    {
+        foreach ($content as $valuesToBeInserted) {
+            $this->insertValues($this->getUpserter($tableName,$identifiedBy,$valuesToBeInserted),$tableName,$valuesToBeInserted);
+        }
+    }
+
+    public function clearAndInsertTable($tableName, $content)
+    {
         $this->clearTable($tableName);
-        
-        if (count($content)==0) {
+
+        if (count($content) == 0) {
             return;
         }
 
         foreach ($content as $valuesToBeInserted) {
-            
-            $insertBuilder      = new Sql\InsertBuilder($tableName);
-
-            foreach ($this->tableInfo->getDecorateWithNullPlaceHolders($tableName, $valuesToBeInserted) as $columnName => $columnValue) {
-                if ($columnValue == 'NULL' && $this->tableInfo->isNullable($tableName,$columnName)) {
-                    $insertBuilder->addColumn($columnName, 'NULL', true);
-                } else {
-                    $insertBuilder->addColumn($columnName, $columnValue);
-                }
-            }
-            $this->executeSql($insertBuilder->GetSql());
+            $this->insertValues(new Sql\InsertBuilder($tableName),$tableName,$valuesToBeInserted);
         }
-        
-        
     }
 
-    protected function clearTable($table) {
+    protected function clearTable($table)
+    {
         $this->executeSql("TRUNCATE TABLE {$table}");
     }
 
@@ -99,16 +97,16 @@ class DataInserter {
     {
         $this->pdo->exec($sql);
     }
-    
+
     protected function getQuery($sql)
     {
         $result = $this->pdo->query($sql);
-        if ($result){
+        if ($result) {
             return $result->fetchAll(\PDO::FETCH_ASSOC);
         }
         return null;
     }
-}
 
+}
 
 ?>
