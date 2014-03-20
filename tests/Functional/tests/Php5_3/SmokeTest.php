0fbcc47f7dc70ac814ef69b246738f545da34ba6
Malte Riesch <malte.riesch@weareifp.com>
2014-03-20 12:12:31 +0000

    increased mockery version and added conditional running of tests on php version

--- tests/Functional/tests/Php5_3/SmokeTest.php
+++ tests/Functional/tests/Php5_3/SmokeTest.php
@@ -1,5 +1,5 @@
 <?php
-require_once(__DIR__.'/../FunctionalBaseTestCase.php');
+require_once(__DIR__.'/../../FunctionalBaseTestCase.php');
 
 class SmokeTest extends FunctionalBaseTestCase {
     
--- tests/Functional/tests/Php5_4/TraitsSmokeTest.php
+++ tests/Functional/tests/Php5_4/TraitsSmokeTest.php
@@ -1,5 +1,5 @@
 <?php
-require_once(__DIR__.'/../FunctionalBaseTestCaseUsingTraits.php');
+require_once(__DIR__.'/../../FunctionalBaseTestCaseUsingTraits.php');
 
 class TraitsSmokeTest extends FunctionalBaseTestCaseUsingTraits 
 {
