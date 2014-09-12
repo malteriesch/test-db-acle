5dcbcff33639103a3380598aa964690476d01366
Malte Riesch <malte.riesch@weareifp.com>
2014-09-12 14:59:05 +0100

    new function: assertArrayContainsPsv

---
+++ lib/TestDbAcle/Commands/FilterArrayByPsvCommand.php
@@ -0,0 +1,76 @@
+<?php
+namespace TestDbAcle\Commands;
+
+
+class FilterArrayByPsvCommand implements \TestDbAcle\Commands\CommandInterface
+{
+    protected $parser;
+    protected $filterQueue;
+    
+    protected $placeHolders;
+    protected $sourcePsv;
+    
+    protected $expectedData;
+    protected $actualData;
+    protected $actualDataUnfiltered;
+    
+    public function __construct($sourcePsv, $actualData, $placeHolders = array())
+    {
+        $this->placeHolders           = $placeHolders;
+        $this->sourcePsv              = $sourcePsv;
+        $this->actualDataUnfiltered   = $actualData;
+        
+    }
+    
+    public function execute()
+    {
+        $actualData           = array();
+        
+        $parsedTree = new \TestDbAcle\Psv\PsvTree();
+        $parsedTree->addTable(new \TestDbAcle\Psv\Table\Table('default', $this->parser->parsePsv($this->sourcePsv)));
+        
+        $filteredParsedTree   = $this->filterQueue->filterDataTree($parsedTree);
+
+        $expectedData = $filteredParsedTree->getTable('default')->toArray();
+        
+        if (count($expectedData) == 0){
+            $this->expectedData = $expectedData;
+            $this->actualData = $this->actualDataUnfiltered;
+            return;
+        } 
+        
+        $allowedColumns = array_keys($expectedData[0]);
+        foreach ($this->actualDataUnfiltered as $row){
+            $actualData[] = array_intersect_key($row, array_flip($allowedColumns));
+        }
+        $this->expectedData = $expectedData;
+        $this->actualData = $actualData;
+    }
+
+    public function initialise(\TestDbAcle\ServiceLocator $serviceLocator)
+    {
+        $this->parser      = $serviceLocator->get('parser');
+        
+        $this->filterQueue          = new \TestDbAcle\Filter\FilterQueue();
+        $this->filterQueue->addRowFilter(new \TestDbAcle\Filter\PlaceholderRowFilter($this->placeHolders));
+        
+    }
+    
+    /**
+     * @return array An associative array representing the PSV
+     */
+    function getExpectedData()
+    {
+        return $this->expectedData;
+    }
+    
+    /**
+     * @return array An associative array representing the table state as configured by the Psv
+     */
+    function getActualData()
+    {
+        return $this->actualData;
+    }
+    
+ 
+}
